af5bdeb301a6e80e24f6a5cf61ed28ca8fde2cde
matsumotokoki <s17c1124br@s.chibakoudai.jp>
2020-02-07 07:53:13 +0000

    add travis

---
+++ test/travis_ros_install.bash
@@ -0,0 +1,21 @@
+#!/bin/bash -xve
+
+#required packages
+pip install catkin_pkg
+pip install empy
+pip install pyyaml
+pip install rospkg
+
+#ros install
+cd ..
+git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu14.04_server.git
+cd ./ros_setup_scripts_Ubintu14.04_server.git
+bash ./setup0.bash
+bash ./setip1.bash
+
+#catkin setup
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/src
+source /opt/ros/indigo/setup.bash
+catkin_init_workspace
+catkin_make
