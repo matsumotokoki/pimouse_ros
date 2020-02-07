a9842e9a28cab585bc45e312adb437e948b193ca
matsumotokoki <s17c1124br@s.chibakoudai.jp>
2020-02-07 08:10:32 +0000

    fix yml

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -1,22 +1,22 @@
 #!/bin/bash -xve
 
 #required packages
-pip install catkin_pkg
-pip install empy
-pip install pyyaml
-pip install rospkg
+pip2 install catkin_pkg
+pip2 install empy
+pip2 install pyyaml
+pip2 install rospkg
 
 #ros install
 cd ..
-git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu16.04_server.git
-cd ./ros_setup_scripts_Ubintu16.04_server
+git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu14.04_server.git
+cd ./ros_setup_scripts_Ubintu14.04_server
 bash ./setup0.bash
 bash ./setip1.bash
 
 #catkin setup
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
-source /opt/ros/kinetic/setup.bash
+source /opt/ros/indigo/setup.bash
 catkin_init_workspace
 cd ~/catkin_ws
 catkin_make
