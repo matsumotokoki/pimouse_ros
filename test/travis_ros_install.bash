131ed2b4fbc360536005c035d89a7c497bf8c7ba
matsumotokoki <s17c1124br@s.chibakoudai.jp>
2020-02-07 08:32:04 +0000

    add echo

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -1,14 +1,14 @@
 #!/bin/bash -xve
 
 #required packages
-echo "install start"
-pip install catkin_pkg
 echo "empy install start"
 pip install empy
 echo "pyyaml install start"
 pip install pyyaml
 echo "rospkg install start"
 pip install rospkg
+echo "catkin_pkg install start"
+pip install catkin_pkg
 echo "install finish"
 
 #ros install
